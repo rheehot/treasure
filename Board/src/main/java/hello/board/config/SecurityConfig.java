fbd17c211962226494fc8dfd4805c8a920d9f919
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2021-11-03 14:52:32 +0900

    spring security start

---
+++ Board/src/main/java/hello/board/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package hello.board.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+public class SecurityConfig {
+
+    
+}
