34501363533d3eb4ad9e352845cc1ebfa95b8c95
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2021-11-03 15:12:09 +0900

    spring security test

--- Board/src/main/java/hello/board/config/SecurityConfig.java
+++ Board/src/main/java/hello/board/config/SecurityConfig.java
@@ -1,11 +1,28 @@
 package hello.board.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @Slf4j
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //사용자 계정은 admin
+        auth.inMemoryAuthentication().withUser("admin")
+                // 1234 패스워드 인코딩 결과
+                .password("$2a$10$uiRTiJ.drvv2OdjESeJh6uwUijdQHn69b2yAbQpN5fiv.E1HI8HIq")
+                .roles("ADMIN");
+    }
 }
---
+++ Board/src/test/java/hello/board/security/PasswordTest.java
@@ -0,0 +1,27 @@
+package hello.board.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootTest
+public class PasswordTest {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void testEncode() {
+        String password = "1234";
+
+        String enPw = passwordEncoder.encode(password);
+
+        System.out.println("enPw :" + enPw);
+
+        boolean matchResult = passwordEncoder.matches(password, enPw);
+
+        System.out.println("matchResult :" + matchResult);
+    }
+
+}
