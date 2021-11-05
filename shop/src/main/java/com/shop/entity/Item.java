a99b3034e6ecd2ac9466b640c141af19ada46103
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2021-11-05 22:59:31 +0900

    shop startgit add shop

---
+++ shop/src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,24 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class Item {
+
+    private Long id; //상품 코드
+    private String itemName; //상품명
+    private int price; //가격
+    private int stockNumber; //재고수량
+    private String itemDetail; //상품상세설명
+    private ItemSellStatus itemSellStatus; //상품 판매 상태
+    private LocalDateTime regTime; //등록 시간
+    private LocalDateTime updateTime; //수정 시간
+
+}
