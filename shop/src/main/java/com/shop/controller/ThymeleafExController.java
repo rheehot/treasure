0e2764ac858b0e8d14b6470a109b434d3d9ad4cc
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2021-11-07 14:32:37 +0900

    thymeleaf th:href parameter Example

--- shop/src/main/java/com/shop/controller/ThymeleafExController.java
+++ shop/src/main/java/com/shop/controller/ThymeleafExController.java
@@ -91,4 +91,12 @@ public class ThymeleafExController {
     public String thymeleafExample06() {
         return "thymeleafEx/thymeleafEx06";
     }
+
+    @GetMapping(value = "/ex07")
+    public String thymeleafExample07(String param1, String param2, Model model) {
+        model.addAttribute("param1", param1);
+        model.addAttribute("param2", param2);
+
+        return "thymeleafEx/thymeleafEx07";
+    }
 }
