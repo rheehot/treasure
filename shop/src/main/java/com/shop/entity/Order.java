19ae27d644a59d41b8cef3ca5c5e553f71e5f26e
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2021-11-08 16:02:39 +0900

    주문 엔티티 지정

---
+++ shop/src/main/java/com/shop/entity/Order.java
@@ -0,0 +1,39 @@
+package com.shop.entity;
+
+import com.shop.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member; // 한명의 회원은 여러 번 주문을 할 수 있기 때문에 다대일 매핑.
+
+    private LocalDateTime orderDate; //주문일
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus; //주문상태
+    @OneToMany(mappedBy = "order")
+    // 주문상품 엔티티와 일대 다 매핑. 연관관계 주인 설정. mappedBy = order는 OrderItem 클래스에 있는 Order에 의해 관리된다는 의미.
+    private List<OrderItem> orderItems = new ArrayList<>(); // 하나의 주문이 여러 개의 주문 상품을 갖으므로 List 사용.
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+
+
+}
---
+++ shop/src/main/java/com/shop/entity/OrderItem.java
@@ -0,0 +1,36 @@
+package com.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item; //하나의 상품은 여러 주문 상품으로 들어갈 수 있으므로 주문 상품 기준 다대일 매핑.
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order; //한번의 주문에 여러 개의 상품을 주문할 수 있으므로, 주문 상품 기준 다대일 매핑.
+
+    private int orderPrice;
+
+    private int count;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+
+
+}
