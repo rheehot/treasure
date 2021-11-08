fbb05f958d2f66a3c6ea84e1c446749416310510
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2021-11-08 21:43:02 +0900

    commit

--- Board/src/main/java/hello/board/config/SecurityConfig.java
+++ Board/src/main/java/hello/board/config/SecurityConfig.java
@@ -45,12 +45,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new LoginSuccessHandler(passwordEncoder());
     }
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        //사용자 계정은 admin
-//        auth.inMemoryAuthentication().withUser("admin")
-//                // 1234 패스워드 인코딩 결과
-//                .password("$2a$10$uiRTiJ.drvv2OdjESeJh6uwUijdQHn69b2yAbQpN5fiv.E1HI8HIq")
-//                .roles("ADMIN");
-//    }
 }
