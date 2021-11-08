57e98e0486c5b9a875ef0029c82ff88641921c70
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2021-11-08 10:02:22 +0900

    중복회원가입 처리

--- shop/src/main/java/com/shop/controller/MemberController.java
+++ shop/src/main/java/com/shop/controller/MemberController.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/members")
 @Controller
@@ -25,11 +28,29 @@ public class MemberController {
         return "member/memberForm";
     }
 
-    @PostMapping(value = "/new")
-    public String memberForm(MemberFormDto memberFormDto) {
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        memberService.saveMember(member);
+//    @PostMapping(value = "/new")
+//    public String memberForm(MemberFormDto memberFormDto) {
+//        Member member = Member.createMember(memberFormDto, passwordEncoder);
+//        memberService.saveMember(member);
+//
+//        return "redirect:/";
+//    }
 
+    @PostMapping(value = "/new")
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model) {
+        //검증. 에러가 있으면, memberForm 으로 이동.
+        if (bindingResult.hasErrors()) {
+            return "member/memberForm";
+        }
+
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e) {
+            //중복 회원가입 예외 발생 시 에러 메세지를 뷰로 전달.
+            model.addAttribute("errorMessage", e.getMessage());
+            return "member/memberForm";
+        }
         return "redirect:/";
     }
 
