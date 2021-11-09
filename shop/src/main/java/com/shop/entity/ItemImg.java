edff1b0b83dc8c0917755ec2e447eb1b7a55af68
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2021-11-09 10:02:55 +0900

    이미지 엔티티 생성

---
+++ shop/src/main/java/com/shop/entity/ItemImg.java
@@ -0,0 +1,39 @@
+package com.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "item_img")
+@Getter
+@Setter
+public class ItemImg extends BaseEntity {
+
+    @Id
+    @Column(name = "item_img_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String imgName; //이미지 파일명
+
+    private String oriImgName; //원본 이미지 파일명
+
+    private String imgUrl; //이미지 조회 경로
+
+    private String repimgYn; //대표 이미지 여부
+
+    @ManyToOne(fetch = FetchType.LAZY) // 상품 엔티티와 다대일 단방향 매핑.
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    //원본 이미지 파일명, 업데이트 할 이미지 파일명, 이미지 경로를 파라미터로 입력받은 후, 이미지 정보 업데이트 하는 메소드.
+    public void updateItemImg(String oriImgName, String imgName, String imgUrl) {
+        this.oriImgName = oriImgName;
+        this.imgName = imgName;
+        this.imgUrl = imgUrl;
+    }
+
+
+}
