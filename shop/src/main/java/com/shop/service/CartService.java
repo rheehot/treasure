70e54ad5dd546aad43494202937956e293a0d1c2
Kim, Sunho <63573287+kimsunho940904@users.noreply.github.com>
2021-11-11 16:08:04 +0900

    장바구니 수정

--- shop/src/main/java/com/shop/service/CartService.java
+++ shop/src/main/java/com/shop/service/CartService.java
@@ -13,10 +13,12 @@ import com.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.util.StringUtils;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -70,4 +72,23 @@ public class CartService {
 
         return cartDetailDtoList;
     }
+
+    @Transactional
+    public boolean validateCartItem(Long cartItemId, String email) {
+        Member curMember = memberRepository.findByEmail(email); // 현재 로그인 한 회원 조회.
+        CartItem cartItem = cartItemRepository.findById(cartItemId).orElseThrow(EntityNotFoundException::new);
+        Member savedMember = cartItem.getCart().getMember();
+
+        if (!StringUtils.equals(curMember.getEmail(), savedMember.getEmail())) {
+            return false;
+        }
+        return true;
+    }
+
+    public void updateCartItemCount(Long cartItemId, int count) {
+        CartItem cartItem = cartItemRepository.findById(cartItemId).orElseThrow(EntityNotFoundException::new);
+        cartItem.updateCount(count);
+    }
+
+
 }
